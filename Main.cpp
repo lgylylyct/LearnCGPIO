92cca168f7bc437b27c901e6504244c0c091f5a5
linguoyu <504971704lgy@gmail.com>
2025-03-23 15:32:45 +0800

    2-2

--- Main.cpp
+++ Main.cpp
@@ -16,17 +16,80 @@
 // clang-format on
 
 using namespace std;
+#define numVAOs 1
 
-void init(GLFWwindow *window) {}
+// GLuint : unsigned int
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+using namespace std;
+
+GLuint createShaderProgram()
+{
+
+	// declares two shaders as character strings
+	const char *vshaderSource =
+		"#version 410 \n"
+		"void main(void) \n"
+		// sets a vertex's coordinate position in 3D space
+		// origin location : (0,0,0)
+		"{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}";
+
+	const char *fshaderSource =
+		"#version 410 \n"
+		"out vec4 color; \n"
+		"void main(void) \n"
+		"{color = vec4(1.0, 0.0, 1.0, 1.0);}";
+
+	// glCreateShader : generates the two shaders of types GL_VERTEX_SHADER and GL_FRAGMENT_SHADER
+	// OpenGL creates each shader object, and returns an integer ID for each
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// loads the GLSL code from the strings into the empty shader objects
+	// glShaderSource(ShaderObject, NumberOfStrings : 1, ArrayOfPointers, -)
+	glShaderSource(vShader, 1, &vshaderSource, nullptr);
+	glShaderSource(fShader, 1, &fshaderSource, nullptr);
+
+	// the shaders are each compiled
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	// the integer ID of a program object
+	GLuint vfProgram = glCreateProgram();
+
+	// attatches each of the shaders to the program object
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	// requests that the GLSL compiler ensure that the shaders are compatible
+	glLinkProgram(vfProgram);
+
+	glDeleteShader(vShader);
+	glDeleteShader(fShader);
+
+	return vfProgram;
+}
+
+void init(GLFWwindow *window)
+{
+	renderingProgram = createShaderProgram();
+
+	// VAO : Vertex Array Objects, OpenGL requires at least one VAO
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
 
 void display(GLFWwindow *window, double currentTime)
 {
-	glClearColor(1.0, 0.0, 0.0, 1.0);
+	// loads the program containing the two compiled shaders into the OpenGL pipeline stages (onto the GPU)
+	glUseProgram(renderingProgram);
 
-	// void glClear(GLbitfield mask)
-	// GL_COLOR_BUFFERE_BIT : references the color buffer
-	// glClear() : clears all of color buffers that OpenGL has
-	glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(50.0f);
+
+	// initiates pipeline processing
+	// mode: GL_POINTS, from 0, one (point)
+	glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(void)
@@ -35,37 +98,26 @@ int main(void)
 	{
 		exit(EXIT_FAILURE);
 	}
-
-	// OpenGL version 2.0
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
-	// monitor, share: for full screen moce and resource sharing
-	GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter2 - program1", nullptr, nullptr);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // I don't know what this does
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);		   // and neither this
+	GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter2 - program2", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Load GLAD so it configures OpenGL
 	gladLoadGL();
+	glfwSwapInterval(1);
 
 	// vertical synchronization (VSyc)
 	glfwSwapInterval(1);
 
-	// includes a reference to the GLFW window object
 	init(window);
 
-	// rendering loop
-	// glfwWindowShouldClose : makes the loop terminate when GLFW detects an sould-close event
 	while (!glfwWindowShouldClose(window))
 	{
-
-		// includes a reference to the GLFW windows object and the current time
 		display(window, glfwGetTime());
-
-		// vertical synchronization (VSyc)
-		// paints the screen
 		glfwSwapBuffers(window);
-
-		// handles other window-related events
 		glfwPollEvents();
 	}
 
